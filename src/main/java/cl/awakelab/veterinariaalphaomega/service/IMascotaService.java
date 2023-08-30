56adb3a9c9d2233636ce3e24f1c4d423af2ea90b
Diego Mansilla <diegomansilla33@gmail.com>
2023-08-29 20:50:24 -0400

    Mascota listo

--- src/main/java/cl/awakelab/veterinariaalphaomega/service/IMascotaService.java
+++ src/main/java/cl/awakelab/veterinariaalphaomega/service/IMascotaService.java
@@ -1,4 +1,12 @@
 package cl.awakelab.veterinariaalphaomega.service;
 
+import cl.awakelab.veterinariaalphaomega.entity.Mascota;
+
+import java.util.List;
+
 public interface IMascotaService {
+    public List<Mascota> listarMascota();
+    public Mascota crearMascota(Mascota alumno);
+    public Mascota actualizarMascota(Mascota alumno);
+    public void eliminarMascota(int idMascota);
 }
--- src/main/java/cl/awakelab/veterinariaalphaomega/service/serviceimpl/MascotaServiceImpl.java
+++ src/main/java/cl/awakelab/veterinariaalphaomega/service/serviceimpl/MascotaServiceImpl.java
@@ -1,4 +1,28 @@
 package cl.awakelab.veterinariaalphaomega.service.serviceimpl;
 
-public class MascotaServiceImpl {
+import cl.awakelab.veterinariaalphaomega.entity.Mascota;
+import cl.awakelab.veterinariaalphaomega.service.IMascotaService;
+
+import java.util.List;
+
+public class MascotaServiceImpl implements IMascotaService {
+    @Override
+    public List<Mascota> listarMascota() {
+        return null;
+    }
+
+    @Override
+    public Mascota crearMascota(Mascota alumno) {
+        return null;
+    }
+
+    @Override
+    public Mascota actualizarMascota(Mascota alumno) {
+        return null;
+    }
+
+    @Override
+    public void eliminarMascota(int idMascota) {
+
+    }
 }
