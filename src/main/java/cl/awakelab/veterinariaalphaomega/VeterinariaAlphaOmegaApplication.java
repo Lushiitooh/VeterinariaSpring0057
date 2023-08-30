760b994579a3c9af6d1c1b80fe240f005f93baf7
Diego Mansilla <diegomansilla33@gmail.com>
2023-08-29 20:15:03 -0400

    Initial commit

---
+++ src/main/java/cl/awakelab/veterinariaalphaomega/VeterinariaAlphaOmegaApplication.java
@@ -0,0 +1,13 @@
+package cl.awakelab.veterinariaalphaomega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinariaAlphaOmegaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinariaAlphaOmegaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/awakelab/veterinariaalphaomega/VeterinariaAlphaOmegaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.awakelab.veterinariaalphaomega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinariaAlphaOmegaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
